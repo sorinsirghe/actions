a45359316b0d95995aca84b9adb702a0b120c21e
Sorin Sirghe <sorinsirghe@protonmail.com>
2023-03-14 22:09:56 +0200

    First commit

---
+++ Functions/functions.py
@@ -0,0 +1,4 @@
+from random import randint
+
+def random_number():
+    return randint(1, 6)
---
+++ Tests/test_functions.py
@@ -0,0 +1,7 @@
+from Functions.functions import random_number
+import pytest
+
+def test_random():
+    number = random_number()
+
+    assert number % 2 == 0, "Number is not even!"
