c4b1ab31a55d0be9a89662b15db28023b8e19614
Sorin Sirghe <sorinsirghe@protonmail.com>
2023-03-14 22:30:43 +0200

    Action test

--- Functions/functions.py
+++ Functions/functions.py
@@ -1,4 +1,10 @@
 from random import randint
 
+
 def random_number():
     return randint(1, 6)
+
+
+def random_something():
+    x = []
+    return x.append(10).append(20).append(60).append(20).append(90).append(20).append(20)
